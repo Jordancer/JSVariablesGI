e9e6a27d06f36b27551d8c7ab80092505d550f5e
Jordan Dancer <112099523+Jordancer@users.noreply.github.com>
2022-09-15 19:47:45 -0400

    Medium Completed

---
+++ Medium/shout.js
@@ -0,0 +1,15 @@
+let p = prompt("Say Hello, Just Dont Shout.");
+let up = "HELLO";
+let lo = "hello";
+let reg = "Hello";
+
+if (p === up) {
+  alert("Now Why Would You Yell At Me, I Thought We Just Talked About This.");
+} else if (p === lo) {
+  // {break} {
+  alert("Speak Up I Cant Hear You.");
+} else if (p === reg) {
+  alert("Hello");
+} else {
+  alert("You Dont Follow Directions Well.");
+}
