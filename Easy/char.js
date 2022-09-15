a6fa480a0a939727f427f2a4c9fd7a0a03e11a63
Jordan Dancer <112099523+Jordancer@users.noreply.github.com>
2022-09-15 17:38:10 -0400

    Easy Completed

---
+++ Easy/char.js
@@ -0,0 +1,4 @@
+let x = "Jodie"; let y = "Langston"; let yl = y.length; let xl = x.length; let z = yl - xl;
+
+console.log( 
+   "The name " + y + " is longer than the name " + x + " by " + z + " characters");
